51d88314b07dc8fd3d2c87ade5522c4bb01eb441
AlexisSG96 <32774104+AlexisSG96@users.noreply.github.com>
2018-10-08 13:49:02 -0700

    Update button.py

--- button.py
+++ button.py
@@ -5,11 +5,13 @@ Date: October 8, 2018
 Email: AlexisSG96@csu.fullerton.edu
 """
 import pygame.font
+from startup import Startup
 
 
 class Button:
     def __init__(self, ai_settings, screen, msg):
-        """Create the play button"""
+        """Create the play button."""
+        self.startup = Startup(screen)
         self.ai_settings = ai_settings
         self.screen = screen
         self.screen_rect = screen.get_rect()
@@ -18,6 +20,7 @@ class Button:
         self.width, self.height = 200, 50
         self.button_color = (0, 255, 0)
         self.text_color = (255, 255, 255)
+        self.black = (0, 0, 0)
         self.font = pygame.font.SysFont(None, 48)
 
         # Build the button's rect object and center it
@@ -29,12 +32,15 @@ class Button:
         self.prep_msg(msg)
 
     def prep_msg(self, msg):
-        """Prepare the message"""
-        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        """Prepare the message."""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.black)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
-        """Draw the play button the screen"""
-        self.screen.fill(self.button_color, self.rect)
+        """Draw the play button on the screen."""
+        # Black screen
+        self.screen.fill(self.black)
+        # self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
+        self.startup.draw_images()
